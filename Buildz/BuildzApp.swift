30e1643270e8fd6039f259e43d1eccb1c7f585b4
Justin-OwiStudent <90196387+Justin-OwiStudent@users.noreply.github.com>
2022-09-22 10:44:24 +0200

    Initial Commit

---
+++ Buildz/BuildzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildzApp.swift
+//  Buildz
+//
+//  Created by Justin on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BuildzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
